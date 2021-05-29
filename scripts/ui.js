21dcb21d4d3b1dfb9451b18e32c38b556474cfdf
Nguyen Quang Trai <quangtrai.zeli@gmail.com>
2021-05-29 19:23:45 +0700

    v2

--- scripts/ui.js
+++ scripts/ui.js
@@ -9,17 +9,17 @@ class ChatUI {
     clear(){
         this.list.innerHTML = '';
     };
-    
+
     render(data){
-        const when = dateFns.distanceInWordsToNow(
-            data.created_at.toDate(),
-            { addSuffix:true }
-          );
+        // const when = dateFns.distanceInWordsToNow(
+        //     data.created_at.toDate(),
+        //     { addSuffix:true }
+        //   );
         const html = `
         <li class="list-group-item">
             <span class="username">${data.username}</span>
             <span class="message">${data.message}</span>
-            <div class="time">${when}</div>
+            <div class="time">${data.created_at.toDate()}</div>
         </li>
         `;
         this.list.innerHTML += html;
